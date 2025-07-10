7a3cc5b255976273bcb7ed2d42a33fc91ff3985c
acrinym <acrinym@gmail.com>
2025-07-09 21:57:15 -0400

    Refine error handling in data loader

---
+++ src/Services/RitualDataLoadException.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace RitualOS.Services
+{
+    /// <summary>
+    /// Represents errors that occur while reading or deserializing ritual data.
+    /// </summary>
+    public class RitualDataLoadException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RitualDataLoadException"/> class.
+        /// </summary>
+        /// <param name="message">Description of the error.</param>
+        /// <param name="innerException">Optional underlying exception that caused the failure.</param>
+        public RitualDataLoadException(string message, Exception? innerException = null)
+            : base(message, innerException)
+        {
+        }
+    }
+}
