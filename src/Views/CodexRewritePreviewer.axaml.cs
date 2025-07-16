6f20a47401aa6b24f8fdb43fd1e7140c71bc5e04
acrinym <acrinym@gmail.com>
2025-07-16 15:19:14 +0000

    Fixes

---
+++ src/Views/CodexRewritePreviewer.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace RitualOS.Views
+{
+    public partial class CodexRewritePreviewer : UserControl
+    {
+        public CodexRewritePreviewer()
+        {
+            InitializeComponent();
+        }
+    }
+}
