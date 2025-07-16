e9bcf28d8e52e7975061ee10d78ea126edc2e557
acrinym <acrinym@gmail.com>
2025-07-16 15:35:06 +0000

    BIGFIX (not tivoli or ibm)

---
+++ src/Views/Wizards/RitualTemplateBuilder.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace RitualOS.Views.Wizards
+{
+    public partial class RitualTemplateBuilder : UserControl
+    {
+        public RitualTemplateBuilder()
+        {
+            InitializeComponent();
+        }
+    }
+}
