af74078bb07298572884e043271a5610b0a8ce3d
acrinym <acrinym@gmail.com>
2025-07-18 01:55:45 -0400

    Add moon phase and ingredient analytics

---
+++ src/Styles/Themes/Theme.Forest.xaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace RitualOS.Styles.Themes
+{
+    public partial class Theme_Forest : ResourceDictionary
+    {
+        public Theme_Forest()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
